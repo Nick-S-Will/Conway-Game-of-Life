b53ce532ccfedd21484a239db7c3c774d9194c96
Nicholas Williams <pizzafeet70@gmail.com>
2020-12-09 01:20:30 -0500

    It works now

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -1,11 +1,10 @@
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.util.Dictionary;
-import java.util.Hashtable;
-import java.util.Vector;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -17,12 +16,12 @@ import javax.swing.Timer;
 
 public class GameOfLife {
 	static JFrame sizeSelect, game, start;
-	static JSpinner sizeX, sizeY;
+	static JSpinner sizex, sizey;
 	static JButton startButton;
-	static Timer t = new Timer(200, new Update());
-	static Font a = new Font("Times New Roman", Font.BOLD, 60);
-	static Dictionary<Vector<Integer>, JButton> cells = new Hashtable<>();
+	static JButton[][] cells;
 	static boolean[][] next;
+	static Font a = new Font("Times New Roman", Font.BOLD, 60);
+	static Timer t = new Timer(200, new Update());
 	static int width, height;
 	static boolean mouseDown;
 	
@@ -35,15 +34,13 @@ public class GameOfLife {
 
 		JPanel spinners = new JPanel();
 		spinners.setLayout(new GridLayout(1, 2, 5, 5));
-		sizeX = new JSpinner(new SpinnerNumberModel(50, 5, 50, 1));
-		sizeY = new JSpinner(new SpinnerNumberModel(50, 5, 50, 1));
-		sizeX.setFont(a);
-		sizeY.setFont(a);
-		spinners.add(sizeX);
-		spinners.add(sizeY);
+		sizex = new JSpinner(new SpinnerNumberModel(50, 5, 50, 1));
+		sizey = new JSpinner(new SpinnerNumberModel(50, 5, 50, 1));
+		sizex.setFont(a);
+		sizey.setFont(a);
+		spinners.add(sizex);
+		spinners.add(sizey);
 		sizeSelect.add(spinners);
-
-
 
 		JButton confirm = new JButton("Confirm");
 		confirm.addMouseListener(new BH());
@@ -54,8 +51,8 @@ public class GameOfLife {
 	}
 
 	public GameOfLife() {
-		width = (int) sizeX.getValue();
-		height = (int) sizeY.getValue();
+		width = (int) sizex.getValue();
+		height = (int) sizey.getValue();
 		int grid = Math.round(800f / height);
 
 		start = new JFrame();
@@ -73,18 +70,23 @@ public class GameOfLife {
 		game.setLocationRelativeTo(null);
 		game.setLayout(new GridLayout(height, width));
 
+		cells = new JButton[width][height];
 		next = new boolean[width][height];
 
 		for (int y = 0; y < height; y++) {
 			for (int x = 0; x < width; x++) {
-				JButton clone = new JButton();
+				StringBuilder xTxt = new StringBuilder(Integer.toString(x));
+				StringBuilder yTxt = new StringBuilder(Integer.toString(y));
+				while (xTxt.length() < 2) xTxt.insert(0, "0");
+				while (yTxt.length() < 2) yTxt.insert(0, "0");
+
+				JButton clone = new JButton(xTxt.toString() + yTxt);
 				clone.setFont(new Font("Arial", Font.PLAIN, 0));
 				clone.setForeground(Color.BLACK);
 				clone.setBackground(Color.BLACK);
 				clone.addMouseListener(new BH());
 
-				var index = new Vector<Integer>(x, y);
-				cells.put(index, clone);
+				cells[x][y] = clone;
 				next[x][y] = false;
 				game.add(clone);
 			}
@@ -95,6 +97,7 @@ public class GameOfLife {
 	}
 
 	private static class Update implements ActionListener {
+		@Override
 		public void actionPerformed(ActionEvent e) {
 			int liveCount = 0;
 
@@ -114,7 +117,7 @@ public class GameOfLife {
 
 			for (int y = 0; y < height; y++) {
 				for (int x = 0; x < width; x++) {
-					setColor((JButton) e.getSource(), next[x][y] ? Color.WHITE : Color.BLACK);
+					setColor(x, y, next[x][y] ? Color.WHITE : Color.BLACK);
 					if (next[x][y]) liveCount++;
 				}
 			}
@@ -125,29 +128,27 @@ public class GameOfLife {
 				startButton.setText("Start");
 			}
 		}
-
-		private int surround(int x, int y) {
-			int count = 0;
-
-			for (int yIt = -1; yIt < 2; yIt++) {
-				for (int xIt = -1; xIt < 2; xIt++) {
-					if (!(xIt == 0 && yIt == 0)) {
-						int relativeX = ((x + xIt) % width + width) % width;
-						int relativeY = ((y + yIt) % height + height) % height;
-
-						if (cells.get(new Vector<Integer>(relativeX, relativeY)).getBackground()
-								== Color.WHITE) count++;
-					}
-				}
-			}
-
-			return count;
-		}
 	}
 
-	private static class BH implements MouseListener, Cloneable {
-		public void ButtonPress(JButton button) {
-			switch (button.getText()) {
+	private static int surround(int x, int y) {
+		int count = 0;
+
+		for (int yIt = -1; yIt < 2; yIt++) {
+			for (int xIt = -1; xIt < 2; xIt++) {
+				if (!(xIt == 0 && yIt == 0)) {
+					int relativeX = ((x + xIt) % width + width) % width, relativeY = ((y + yIt) % height + height) % height;
+
+					if (cells[relativeX][relativeY].getBackground() == Color.WHITE) count++;
+				}
+			}
+		}
+
+		return count;
+	}
+
+	private static class BH implements MouseListener {
+		public void ButtonPress(String s) {
+			switch (s) {
 			case "Confirm":
 				sizeSelect.setVisible(false);		
 				mouseDown = false;
@@ -156,8 +157,7 @@ public class GameOfLife {
 			case "Start":
 				for (int y = 0; y < height; y++) {
 					for (int x = 0; x < width; x++) {
-						if (cells.get(new Vector<Integer>(x, y)).getBackground() == Color.WHITE)
-							next[x][y] = true;
+						if (cells[x][y].getBackground() == Color.WHITE) next[x][y] = true;
 					}
 				}
 				startButton.setText("Stop");
@@ -171,31 +171,37 @@ public class GameOfLife {
 				break;
 			default:
 				if (!t.isRunning()) {
-					setColor(button, button.getBackground().getRed() == 0 ?
-							Color.WHITE : Color.BLACK);
+					int x = Integer.parseInt(s.substring(0, 2)), y = Integer.parseInt(s.substring(2, 4));
+					if (cells[x][y].getBackground() == Color.BLACK) setColor(x, y, Color.WHITE);
+					else setColor(x, y, Color.BLACK);
 				}
 				break;
 			}
 		}
 
+		@Override
 		public void mouseClicked(MouseEvent e) {}
+		@Override
 		public void mousePressed(MouseEvent e) {
 			mouseDown = true;
 			if (e.getSource() instanceof JButton) 
-				ButtonPress((JButton) e.getSource());
+				ButtonPress(((JButton) e.getSource()).getText());
 		}
+		@Override
 		public void mouseReleased(MouseEvent e) {
 			mouseDown = false;
 		}
+		@Override
 		public void mouseEntered(MouseEvent e) {
 			if (e.getSource() instanceof JButton && mouseDown) 
-				ButtonPress((JButton) e.getSource());
+				ButtonPress(((JButton) e.getSource()).getText());
 		}
+		@Override
 		public void mouseExited(MouseEvent e) {}
 	}
 
-	private static void setColor(JButton button, Color c) {
-		button.setBackground(c);
-		button.setForeground(c);
+	private static void setColor(int x, int y, Color c) {
+		cells[x][y].setBackground(c);
+		cells[x][y].setForeground(c);
 	}
 }
